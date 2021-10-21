7ca88e85a504cdfe7b18c9ca820751d64cda8383
Wayne Chan <wchan.it@outlook.com>
2021-10-20 23:54:51 -0500

    Add ColoringProblem

---
+++ src/main/java/coloringproblem/App.java
@@ -0,0 +1,22 @@
+package coloringproblem;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                { 0, 1, 0, 1, 0 },
+                { 1, 0, 1, 1, 0 },
+                { 0, 1, 0, 1, 0 },
+                { 1, 1, 1, 0, 1 },
+                { 0, 0, 0, 1, 0 }
+        };
+
+        // no solution for 2
+//        int numColors = 2;
+        int numColors = 3;
+
+        ColoringProblem coloringProblem  = new ColoringProblem(numColors, matrix);
+        coloringProblem.solve();
+    }
+}
---
+++ src/main/java/coloringproblem/ColoringProblem.java
@@ -0,0 +1,69 @@
+package coloringproblem;
+
+public class ColoringProblem {
+
+    private int numOfVertices;
+    private int numOfColors;
+    private int[] colors;
+    private int[][] adjacencyMatrix;
+
+    public ColoringProblem(int numOfColors, int[][] adjacencyMatrix) {
+        this.numOfColors = numOfColors;
+        this.adjacencyMatrix = adjacencyMatrix;
+        this.numOfVertices = adjacencyMatrix.length;
+        this.colors = new int[numOfVertices];
+    }
+
+    public void solve() {
+
+        if (solveProblem(0)) {
+            showResults();
+        } else {
+            System.out.println("No solution...");
+        }
+    }
+
+    private boolean solveProblem(int nodeIndex) {
+        if (nodeIndex == numOfVertices) {
+            return true;
+        }
+
+        for (int colorIndex = 1; colorIndex <= numOfColors; colorIndex++) {
+            // color is valid if we are able to assign that color to that given node
+            if (isColorValid(nodeIndex, colorIndex)) {
+                colors[nodeIndex] = colorIndex;
+
+                // try and solve the problem recursively the next node
+                // if it is possible, we will return true
+                if (solveProblem(nodeIndex + 1)) {
+                    return true;
+                }
+
+                // backtrack!!!
+                // this means we just increment the color index because we are not able
+                // to assign that colorIndex to the given node
+
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isColorValid(int nodeIndex, int colorIndex) {
+        for (int i = 0; i < numOfVertices; i++) {
+            // there is a connection with neighboring nodes
+            //  adjacen nodes with the same color
+            if (adjacencyMatrix[nodeIndex][i] == 1 && colorIndex == colors[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void showResults() {
+        for (int i = 0; i < numOfVertices; i++) {
+            System.out.println("Node " + (i + 1) + " has color index: " + colors[i]);
+        }
+    }
+}
